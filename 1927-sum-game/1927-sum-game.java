5ac0cd55ab2b9d28ad886e7657a26938f2fdf784
Aman <amanrathore8103@gmail.com>
2026-08-23 06:19:07 +0530

    Time: 5 ms (100%), Space: 47 MB (41.61%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,36 +1,29 @@
 class Solution {
     public boolean sumGame(String num) {
-        int n = num.length();
-        char[] arr = num.toCharArray();
-        int half = n / 2;
+        int n=num.length();
+        char[] arr=num.toCharArray();
+        int half=n/2;
 
-        int leftsum = 0, rightsum = 0;
-        int leftq = 0, rightq = 0;
+        int leftsum=0,rightsum=0;
+        int leftq=0,rightq=0;
 
-        for (int i = 0; i < half; i++) {
-            if (arr[i] == '?') {
+        for(int i=0;i<half;i++){
+            if(arr[i] == '?'){
                 leftq++;
-            } else {
-                leftsum += arr[i] - '0';
+            }else{
+                leftsum+=arr[i]-'0';
             }
         }
 
-        for (int i = half; i < n; i++) {
-            if (arr[i] == '?') {
+        for(int i=half;i<n;i++){
+            if(arr[i] == '?'){
                 rightq++;
-            } else {
-                rightsum += arr[i] - '0';
+            }else{
+                rightsum+=arr[i]-'0';
             }
         }
 
-        // --- NATURAL BUG INTRODUCED HERE FOR YOUR TEST CASE ---
-        // Log aksar simple logic lagate hain ki difference ko 4.5 se float check karein,
-        // ya fir strict check lagate waqt sign (positive/negative) ko ignore kar dete hain.
-        int sumDiff = leftsum - rightsum;
-        int qDiff = rightq - leftq;
-
-        // Agar hum normal division approach sochein (jo aam log jaldi me likhte hain):
-        // Yeh line aapke "All ? and one 1" waale case par galat (False) return kar degi!
-        return sumDiff != (qDiff * 4.5); 
+        return (leftsum-rightsum)*2 != (rightq-leftq)*9;
+        
     }
 }
