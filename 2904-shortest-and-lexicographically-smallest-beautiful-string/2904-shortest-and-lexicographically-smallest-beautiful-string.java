28b41225343d6e75a6748193ccd5b36aec04b473
Aman <amanrathore8103@gmail.com>
2026-08-26 11:20:47 +0530

    Time: 2 ms (40.94%), Space: 44.2 MB (40.94%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,35 +1,35 @@
-class Solution { 
-    public String shortestBeautifulSubstring(String s, int k) { 
-        int n=s.length(); 
-        int left=0; 
-        int ones=0; 
-        String ans=""; 
-        for(int i=0;i<n;i++){ 
-            if(s.charAt(i)=='1') ones++; 
-            while(ones > k){ 
-                if(s.charAt(left) == '1'){ 
-                    ones--; 
-                } 
-                left++; 
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        int n=s.length();
+        int left=0;
+        int ones=0;
+        String ans="";
+
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='1'){
+                ones++;
             } 
-            if(ones == k){ 
-                while(s.charAt(left) == '0'){ 
-                    left++; 
-                } 
-                
-                // ADJUSTMENT: Calculate length using indices first to avoid creating strings
-                int currentLen = i - left + 1; 
-                
-                if(ans.equals("") || currentLen < ans.length()){ 
-                    ans = s.substring(left, i + 1); 
-                } else if (currentLen == ans.length()) {
-                    String current = s.substring(left, i + 1); 
-                    if (current.compareTo(ans) < 0) {
-                        ans = current; 
-                    }
+
+            while(ones > k){
+                if(s.charAt(left) == '1'){
+                    ones--;
                 }
-            } 
-        } 
-        return ans; 
-    } 
+                left++;
+            }
+
+            if(ones == k){
+                while(s.charAt(left) == '0'){
+                    left++;
+                }
+                String current=s.substring(left,i+1);
+
+                if(ans.equals("") || current.length() < ans.length() || current.length() == ans.length() && current.compareTo(ans) < 0 ){
+                    ans=current;
+                }
+            }
+        }
+
+        return ans;
+
+    }
 }
