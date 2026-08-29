327d586659bb28718f0203acdf2b2ef13318ca3c
Aman <amanrathore8103@gmail.com>
2026-08-30 02:09:47 +0530

    Time: 0 ms (100%), Space: 45.6 MB (35.02%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.java
+++ 2951-find-the-peaks/2951-find-the-peaks.java
@@ -4,7 +4,10 @@ class Solution {
         ArrayList<Integer> ans=new ArrayList<>();
 
         for(int i=1;i<n-1;i++){
-            if((arr[i-1]<arr[i]) && (arr[i]>arr[i+1]) ) ans.add(i);
+            if((arr[i-1]<arr[i]) && (arr[i]>arr[i+1]) ){
+                ans.add(i);
+                i++;
+            }
         }
         return ans;
     }
