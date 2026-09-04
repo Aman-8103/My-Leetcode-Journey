f775268ac56c293bb21f17ec84ee52c3d639de5f
Aman <amanrathore8103@gmail.com>
2026-09-04 13:23:58 +0530

    Time: 4 ms (89.19%), Space: 132.8 MB (36.68%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstStableIndex(int[] arr, int k) {
+        int n=arr.length;
+
+        int[] premin=new int[n];
+        premin[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--){ // this for computing min from i-(n-1)
+            premin[i]=Math.min(arr[i],premin[i+1]);
+        }
+
+        int max=arr[0];
+        for(int i=0;i<n;i++){ //normal compute max and subtract with premin[i];
+            max=Math.max(arr[i],max);
+            int min=premin[i];
+
+            if(max-min <= k) return i;
+        }
+        return -1;
+    }
+}
