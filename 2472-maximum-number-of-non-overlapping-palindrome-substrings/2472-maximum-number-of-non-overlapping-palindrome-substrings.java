c4d05463fbcfb8d54551364fad9a274a760be0e7
Aman <amanrathore8103@gmail.com>
2026-09-16 01:53:12 +0530

    Time: 1 ms (100%), Space: 42.3 MB (100%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,31 +1,35 @@
 class Solution {
+
     public int maxPalindromes(String s, int k) {
-        int n = s.length();
-        boolean[][] isPalindrome = new boolean[n][n];
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+        int ans = 0;
+        int start = 0;
 
-        // Step 1: Precompute all palindrome substrings
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
-                if (s.charAt(i) == s.charAt(j)) {
-                    if (j - i <= 2 || isPalindrome[i + 1][j - 1]) {
-                        isPalindrome[i][j] = true;
-                    }
-                }
+        for (int i = k - 1; i < n; ++i) {
+            int l = i - k + 1;
+            if (l >= start && check(chars, l, i)) {
+                ++ans;
+                start = i + 1;
+                continue;
+            }
+
+            l = i - k;
+            if (l >= start && check(chars, l, i)) {
+                ++ans;
+                start = i + 1;
             }
         }
 
-        // Step 2: DP to find max non-overlapping palindromes of length >= k
-        int[] dp = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            dp[i + 1] = dp[i]; // Carry forward previous max
-            for (int j = i - k + 1; j >= 0; j--) {
-                if (isPalindrome[j][i]) {
-                    dp[i + 1] = Math.max(dp[i + 1], dp[j] + 1);
-                    break; // Optimization: taking the earliest valid start is optimal
-                }
+        return ans;
+    }
+
+    private boolean check(char[] chars, int l, int i) {
+        while (l < i) {
+            if (chars[l++] != chars[i--]) {
+                return false;
             }
         }
-
-        return dp[n];
+        return true;
     }
 }
